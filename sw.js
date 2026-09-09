59a611605f055d0d00b0a1910d3918b31ced7114
akhilkvs07-cmd <akhilkvs07@gmail.com>
2026-09-09 15:26:14 +0530

    Add SmartGuide offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='bis-smartguide-v7';
+const SHELL=['./','./index.html','./smartguide-v7.js','./smartguide-v7.css','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(x=>x.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
